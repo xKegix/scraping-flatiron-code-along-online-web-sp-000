2b13eb1adb7184d4e63f8b2440bae71b6e8cccf4
Keginold Philip <keginoldp@hotmail.com>
2021-04-20 12:06:39 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,21 +1,15 @@
 class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  def self.all
+    @@all
+  end 
 
 
 
